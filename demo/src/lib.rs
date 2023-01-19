b4f35f792fed1f7e6eaa87ce8025e961204a83c5
jinser <cmdr.jv@gmail.com>
2023-01-19 15:16:27 +0800

    true address

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -3,7 +3,7 @@ use slack_flows::send_message_to_channel;
 
 #[no_mangle]
 pub fn run() {
-    if let Some(bnm) = event_received("eth address like") {
+    if let Some(bnm) = event_received("0xC8a8f0C656D21bd619FB06904626255af19663ff") {
         send_message_to_channel("ham-5b68442", "general", bnm.to);
     }
 }
