aea2ca60ba178cf1fcdf718b0c020c82a00da42f
Jinser <cmdr.jv@gmail.com>
2023-02-01 13:16:27 +0800

    update

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,9 +1,11 @@
-use blocknative_flows::event_received;
+use blocknative_flows::listen_to_address;
 use slack_flows::send_message_to_channel;
 
 #[no_mangle]
 pub fn run() {
-    if let Some(bnm) = event_received("0xC8a8f0C656D21bd619FB06904626255af19663ff") {
-        send_message_to_channel("ham-5b68442", "general", bnm.to);
-    }
+    let address = "0xC8a8f0C656D21bd619FB06904626255af19663ff";
+
+    listen_to_address(address, |bnm| {
+        send_message_to_channel("ham-5b68442", "general", bnm.hash);
+    });
 }
